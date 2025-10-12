37235656684146cb120ec7b79d9c5372567daab0
PradeepPs04 <pradeep.ps2004@gmail.com>
2025-10-12 13:27:35 +0530

    add: 11 oct potd

---
+++ October/11.cpp
@@ -0,0 +1,70 @@
+// https://leetcode.com/problems/maximum-total-damage-with-spell-casting/?envType=daily-question&envId=2025-10-12
+
+// top-down (memoization)
+#define ll long long
+
+class Solution {
+private:
+    ll solve(vector<ll>& nums, int idx, unordered_map<ll, ll>& freq, vector<ll>& dp) {
+        if(idx >= nums.size())  return 0;
+
+        if(dp[idx] != -1)   return dp[idx];
+
+        // not pick
+        ll not_pick = solve(nums, idx+1, freq, dp);
+
+        // pick
+        int j = lower_bound(nums.begin()+idx+1, nums.end(), nums[idx]+3) - nums.begin();
+        ll pick = nums[idx] * freq[nums[idx]] + solve(nums, j, freq, dp);
+
+        return dp[idx] = max(pick, not_pick);
+    }
+public:
+    long long maximumTotalDamage(vector<int>& power) {
+        unordered_map<ll, ll>freq;
+        for(auto it:power)  freq[it]++;
+
+        vector<ll>nums;
+        for(auto it:freq)    nums.push_back(it.first);
+
+        sort(nums.begin(), nums.end());
+
+        vector<ll>dp(nums.size(), -1);
+
+        return solve(nums, 0, freq, dp);
+    }
+};
+
+
+// bottom-up (tabulation)
+#define ll long long
+
+class Solution {
+public:
+    long long maximumTotalDamage(vector<int>& power) {
+        unordered_map<ll, ll>freq;
+        for(auto it:power)  freq[it]++;
+
+        vector<ll>nums;
+        for(auto it:freq)    nums.push_back(it.first);
+
+        sort(nums.begin(), nums.end());
+
+        vector<ll>dp(nums.size()+1, 0);
+
+        ll ans = INT_MIN;
+
+        for(int i = nums.size()-1; i >= 0; i--) {
+            ll not_pick = dp[i+1];
+
+            int j = lower_bound(nums.begin() + i + 1, nums.end(), nums[i]+3) - nums.begin();
+            ll pick = nums[i] * freq[nums[i]] + dp[j];
+
+            dp[i] = max(pick, not_pick);
+
+            ans = max(ans, dp[i]);
+        }
+
+        return  ans;
+    }
+};
