1e9ab01b169d8fb88ecb542b383853253d7d18df
PradeepPs04 <148530874+PradeepPs04@users.noreply.github.com>
2026-01-26 11:58:59 +0530

    add: 26 jan 2026 potd

---
+++ 2026_01 January/26.cpp
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/minimum-absolute-difference/?envType=daily-question&envId=2026-01-26
+class Solution {
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        int mini = INT_MAX;
+        for(int i = 1; i < arr.size(); i++) mini = min(mini, arr[i]-arr[i-1]);
+
+        vector<vector<int>>ans;
+        for(int i=1; i<arr.size(); i++) {
+            if(arr[i]-arr[i-1] == mini) ans.push_back({arr[i-1], arr[i]});
+        }
+
+        return ans;
+    }
+};
